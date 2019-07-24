bd7c5270d589715faf02a16ada718a4bb3a9e5cc
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-24 13:19:11 -0400

    Creating model loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Sergio!!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Sergio Rosa',
+    email: 'srosa@gmail.com',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
