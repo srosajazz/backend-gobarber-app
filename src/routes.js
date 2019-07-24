ca16a07ce56a37a78a68f22e235af43f7484340d
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-24 14:51:19 -0400

    Creating User Registration

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Sergio Rosa',
-    email: 'srosa@gmail.com',
-    password_hash: '12345',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
