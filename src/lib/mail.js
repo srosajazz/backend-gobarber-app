5831634a4f01a1c181b756bd46ef3feec52cdaf7
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-28 15:49:27 -0400

    Implementing e-mail template with Handlerbars

--- src/lib/mail.js
+++ src/lib/mail.js
@@ -1,4 +1,8 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+
 import mailConfig from '../config/mail';
 
 class Mail {
@@ -10,6 +14,19 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({}),
+      })
+    );
   }
 
   sendMail(message) {
