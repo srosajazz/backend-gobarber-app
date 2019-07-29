d3e3f8063241cbf605e12e7ae54274238314b731
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-29 18:54:54 -0400

    Implementing Virtual Fields in Scheduling

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -13,7 +13,6 @@ import Appointment from '../models/Appointment';
 class AvailableController {
   async index(req, res) {
     const { date } = req.query;
-
     if (!date) {
       return res.status(400).json({ error: 'Invalid date' });
     }
